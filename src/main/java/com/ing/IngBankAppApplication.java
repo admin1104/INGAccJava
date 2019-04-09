438e77bdd9b26a1a6ac6ee6b252d6120a38a68dc
hariharamohanty@gmail.com <hariharamohanty@gmail.com>
2019-04-09 00:17:35 -0700

    Basic Project Structure
    Basic Project Structure

---
+++ src/main/java/com/ing/IngBankAppApplication.java
@@ -0,0 +1,13 @@
+package com.ing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngBankAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngBankAppApplication.class, args);
+	}
+
+}
