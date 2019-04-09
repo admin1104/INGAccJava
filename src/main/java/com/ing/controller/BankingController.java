fa6217a1a45e650166cbad227944a7f284e11f30
admin1104 <49300434+admin1104@users.noreply.github.com>
2019-04-09 19:22:10 +0530

    commmitting

--- src/main/java/com/ing/controller/BankingController.java
+++ src/main/java/com/ing/controller/BankingController.java
@@ -1,5 +1,7 @@
 package com.ing.controller;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,9 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ing.dto.CreateAccountDto;
 import com.ing.dto.LoginDto;
 import com.ing.dto.LoginResponseDto;
+import com.ing.dto.TransactionDto;
 import com.ing.service.CreateAccountService;
 import com.ing.service.CustomerService;
 import com.ing.service.LoginService;
+import com.ing.service.TransactionService;
+
 
 @RestController
 @RequestMapping("/api/v1")
@@ -29,6 +34,9 @@ public class BankingController {
 
 	@Autowired
 	public CustomerService customerService;
+	
+	@Autowired
+	TransactionService transservice;
 	
 	@PostMapping("/login")
 	public ResponseEntity<LoginResponseDto> processLogin(@RequestBody LoginDto loginRequest) {
@@ -52,6 +60,12 @@ public class BankingController {
 		} else {
 			return ResponseEntity.badRequest().body(responseMessage);
 		}
+	}
+	@PostMapping("/createtransactions")
+	public ResponseEntity<String> createTransaction(@RequestBody TransactionDto transdto) {
+		transservice.saveTransaction(transdto);
+		String responseMessage = "Transaction Saved Suucessfully!";
+		return ResponseEntity.ok().body(responseMessage);
 	}
 	
 }
